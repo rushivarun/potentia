ee5f48c9ca6a8a4f30d9b42c58441306f923b783
rushivarun <varun.muniaplle2017@vitstudent.ac.in>
2019-11-07 00:37:34 +0530

    production ready

--- potentia_backend/potentia_backend/settings.py
+++ potentia_backend/potentia_backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import django_heroku
+# import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
